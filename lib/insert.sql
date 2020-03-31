d0b82985ae61e1402221e2bc81f4257609cf43e2
gavinsharkey <gavinsharkey2016@gmail.com>
2020-03-31 16:18:11 -0400

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,5 +5,7 @@ INSERT INTO subgenres (name) VALUES ('Fantasy'), ('Something else');
 INSERT INTO authors (name) VALUES ('J.K.Rowling'), ('Some guy');
 
 INSERT INTO characters (name) VALUES ('Harry potter'), ('Ronald weasley'), ('Hermione granger'), ('Voldemort'), 
-('1'), ('2'), ('3'), ('4')
+('1'), ('2'), ('3'), ('4');
+
+
 
